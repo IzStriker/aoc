3a2bda09bb28aed5879d7c5c91750f05770d4e52
Isaac George <i.d.george@hotmail.com>
2023-12-05 07:02:30 +0000

    day 5 part 1 done

--- 2023/5/main.py
+++ 2023/5/main.py
@@ -18,14 +18,13 @@ def part1():
             source = lines[i].strip().split(":")[0].split("-")[0]
             dest = lines[i].strip().split(":")[0].split("-")[-1]
             guide[source] = dest.split(" ")[0]
-            maps[current_map] = {}
+            maps[current_map] = []
         elif(lines[i].strip() != ""):
             line = lines[i].strip().split(" ")
             source_start = int(line[0])
             dest_start = int(line[1])
             length = int(line[2])
-            for i in range(length):
-                maps[current_map][dest_start + i] =  source_start + i
+            maps[current_map].append([source_start, dest_start, length])
     
     values = []
     for seed in seeds:
@@ -33,16 +32,75 @@ def part1():
         current_value = seed
         while map in guide:
             current_map = maps[f"{map}-to-{guide[map]}"] 
-            if (current_value in current_map):
-                current_value = current_map[current_value]
+            
+            for value_range in current_map:
+                dest_start = value_range[0]
+                source_start = value_range[1]
+                length = value_range[2]
+
+                if current_value >= source_start and current_value <= source_start + length:
+                    current_value = dest_start + (current_value - source_start)
+                    break
             map = guide[map]
         values.append(current_value)
     
-    print(values)
+    print(min(values))
 
+
+def part2():
+    lines = []
+
+    with open('input.txt', 'r') as f:
+        lines = f.readlines()
+
+    seeds = list(lines[0].strip().split(":")[1].strip().split(" ") | pipe.map(lambda x: int(x)))
+    seed_ranges = []
+    i = 0
+    while i < len(seeds):
+        seed_ranges.append([seeds[i], seeds[i+1]])
+        i += 2
+
+    maps = {}
+    guide = {}
+    current_map = ""
+    for i in range(2, len(lines)):
+        if (":" in lines[i]):
+            current_map = lines[i].strip().split(":")[0].split(" ")[0]
+            source = lines[i].strip().split(":")[0].split("-")[0]
+            dest = lines[i].strip().split(":")[0].split("-")[-1]
+            guide[source] = dest.split(" ")[0]
+            maps[current_map] = []
+        elif(lines[i].strip() != ""):
+            line = lines[i].strip().split(" ")
+            source_start = int(line[0])
+            dest_start = int(line[1])
+            length = int(line[2])
+            maps[current_map].append([source_start, dest_start, length])
+    
+    values = []
+
+    for seed_range in seed_ranges:
+        for seed in range(seed_range[0], seed_range[0] + seed_range[1]):
+            map = "seed"
+            current_value = seed
+            while map in guide:
+                current_map = maps[f"{map}-to-{guide[map]}"] 
+                
+                for value_range in current_map:
+                    dest_start = value_range[0]
+                    source_start = value_range[1]
+                    length = value_range[2]
+
+                    if current_value >= source_start and current_value <= source_start + length:
+                        current_value = dest_start + (current_value - source_start)
+                        break
+                map = guide[map]
+            values.append(current_value)
 
     
+    print(min(values))
+
     
 
 if __name__ == "__main__":
-    part1()
+    part2()
