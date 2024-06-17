feb69b5e1b44d602ce5acdcdb284489e4f2573f3
Isaac George <i.d.george@hotmail.com>
2024-06-17 13:19:48 +0100

    day completed

--- 2023/9/main.py
+++ 2023/9/main.py
@@ -30,5 +30,25 @@ def part1():
         predicted_values.append(current_predicted)
     print(sum(predicted_values))
 
+def part2():
+    lines = []
+    with open("input.txt") as f:
+        lines = f.readlines() | map(lambda x: list(x.strip().split(" ") | map(int)))
+    
+    predicted_values = []
+    for line in lines: 
+        differences = [line]
+        current = line
+        while any(list(current | map(lambda x: x != 0))):
+            current = get_differences(current)
+            differences.append(current)
+        
+        current_predicted = 0
+        for i in reversed(range(len(differences))):
+            current_predicted = differences[i][0] - current_predicted
+        
+        predicted_values.append(current_predicted)
+    print(sum(predicted_values))
+
 if __name__ == "__main__":
-    part1()
+    part2()
